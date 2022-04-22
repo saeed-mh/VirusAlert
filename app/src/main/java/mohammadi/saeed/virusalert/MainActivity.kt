8f7f92503aa356e95e035f2c583341851af858d0
Saeed <Saeedmh8908@gmail.com>
2022-04-22 21:11:30 +0430

    add AboutUsFragment.kt and fragment_about_us.xml

--- app/src/main/java/mohammadi/saeed/virusalert/MainActivity.kt
+++ app/src/main/java/mohammadi/saeed/virusalert/MainActivity.kt
@@ -2,11 +2,18 @@ package mohammadi.saeed.virusalert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun replaceFragment(view: View, action: Int) {
+        Navigation.findNavController(view).navigate(R.id.action_mainFragment_to_aboutUsFragment)
     }
 }
--- app/src/main/java/mohammadi/saeed/virusalert/StatisticsCoronaFragment.kt
+++ app/src/main/java/mohammadi/saeed/virusalert/StatisticsCoronaFragment.kt
@@ -1,13 +1,10 @@
 package mohammadi.saeed.virusalert
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
+ * Use the [StatisticsCoronaFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainFragment : Fragment() {
+class StatisticsCoronaFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,24 +34,7 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val mainActivityBottomNavigationViewBtnNav =
-            view.findViewById<BottomNavigationView>(R.id.aboutUsActivity_bottomNavigationView_BtnNav)
-
-        mainActivityBottomNavigationViewBtnNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.item3 -> {
-                    MainActivity().replaceFragment(view, R.id.action_mainFragment_to_aboutUsFragment)
-                }
-            }
-            return@setOnItemSelectedListener true
-        }
-
+        return inflater.inflate(R.layout.fragment_statistics_corona, container, false)
     }
 
     companion object {
@@ -64,12 +44,12 @@ class MainFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
+         * @return A new instance of fragment StatisticsCoronaFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
+            StatisticsCoronaFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
