c87c71b7e1f0ba9d05bf04e239928b900e6d58f5
Saeed <Saeedmh8908@gmail.com>
2022-04-24 19:24:57 +0430

    using ViewBinding instead of FindViewByID

--- app/src/main/java/mohammadi/saeed/virusalert/SignupFragment.kt
+++ app/src/main/java/mohammadi/saeed/virusalert/SignupFragment.kt
@@ -9,76 +9,33 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.RadioGroup
 import androidx.navigation.Navigation
+import mohammadi.saeed.virusalert.databinding.FragmentSignupBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignupFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignupFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signup, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentSignupBinding.bind(view)
 
-        val signupActivityBtnSignUp = view.findViewById<Button>(R.id.signupActivity_btn_SignUp)
-        val signupActivityCheckBoxVirus =
-            view.findViewById<CheckBox>(R.id.signupActivity_checkBox_Virus)
-        val signupActivityRadioGroupVirusRadioGroup =
-            view.findViewById<RadioGroup>(R.id.signupActivity_radioGroup_VirusRadioGroup)
-
-        signupActivityCheckBoxVirus.setOnCheckedChangeListener { _, isChecked ->
+        binding.signupActivityCheckBoxVirus.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked)
-                signupActivityRadioGroupVirusRadioGroup.visibility = View.VISIBLE
+                binding.signupActivityRadioGroupVirusRadioGroup.visibility = View.VISIBLE
             else
-                signupActivityRadioGroupVirusRadioGroup.visibility = View.INVISIBLE
+                binding.signupActivityRadioGroupVirusRadioGroup.visibility = View.INVISIBLE
         }
 
-        signupActivityBtnSignUp.setOnClickListener {
+        binding.signupActivityBtnSignUp.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_signupFragment_to_statisticsCoronaFragment)
         }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignupFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignupFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
